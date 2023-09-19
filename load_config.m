d920f2ee2c773e47a592f4b31611a8908b747744
tamaskis <tamas.a.kis@outlook.com>
2023-09-18 22:35:52 -0700

    bugfix

--- load_config.m
+++ load_config.m
@@ -5,7 +5,7 @@
 %   config = load_config(file_path)
 %
 % Copyright © 2023 Tamas Kis
-% Last Update: 2023-05-20
+% Last Update: 2023-09-18
 % Website: https://tamaskis.github.io
 % Contact: tamas.a.kis@outlook.com
 %
@@ -45,7 +45,8 @@ function config = load_config(file_path)
     % initializes config dictionary to return
     config = dictionary;
     
-    % populates config dictionary
+    % simplifies struct produces by table2struct, also converting all
+    % fields from chars to strings
     for i = 1:length(fields)
         
         % extracts key and value
@@ -81,7 +82,7 @@ function config = load_config(file_path)
             
             % converts to double or logical array if value elements are 
             % numeric or logical
-            if ~isempty(str2num(value(1)))
+            if isnumeric(value(1)) || islogical(value(1))
                 value = str2num(convertStringsToChars(strjoin(value)));
             end
             
